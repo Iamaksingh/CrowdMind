9282a9012e7d477f89be3f7c13468fbe6f7b6c0b
Aditya Kumar Singh <iamaksingh2004@gmail.com>
2025-08-14 18:18:27 +0530

    removed duplicate file

--- backend/routes/auth.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { signup, login } from "../controllers/authController.js";
-
-const authRoutes = express.Router();
-
-authRoutes.post("/signup", signup);
-authRoutes.post("/login", login);
-
-export default authRoutes;
