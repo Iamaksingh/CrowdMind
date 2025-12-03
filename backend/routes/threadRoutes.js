03e300d795c24403ec37eca94c164adf222c5ec2
Aditya Kumar Singh <iamaksingh2004@gmail.com>
2025-12-03 22:11:13 +0530

    added comment analysis route

--- backend/routes/threadRoutes.js
+++ backend/routes/threadRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import Thread from '../models/Thread.js';
-import { createThread, getThreads, getThreadById, deleteThread, addComment, getThreadByUser, like_unlike } from '../controllers/threadController.js';
+import { createThread, getThreads, getThreadById, deleteThread, addComment, getThreadByUser, like_unlike, getCommentAnalysis } from '../controllers/threadController.js';
 import { upload } from '../config/cloudinary.js';
 import { protect } from '../middleware/authMiddleware.js';
 const router = express.Router();
@@ -13,6 +13,7 @@ router.post('/', protect, upload.single("file"), createThread);
 router.get("/mythreads", protect, getThreadByUser); 
 router.post("/:id/like", protect, like_unlike );
 router.post("/:id/comments", protect, addComment);
+router.get('/:threadId/comments/:commentIndex/analysis', getCommentAnalysis);
 router.get('/:id', getThreadById);
 router.delete('/:id', protect, deleteThread);
 
